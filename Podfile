f6624d13ccd6708c8d8441912602b3d77e6d6d44
Łukasz Adamczak <lukasz@czak.pl>
2013-06-04 11:34:19 +0200

    Setup Cocoapods & add MWPhotoBrowser

---
+++ Podfile
@@ -0,0 +1,2 @@
+platform :ios
+pod 'MWPhotoBrowser', '~> 1.0.1'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,32 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// MBProgressHUD
+#define COCOAPODS_POD_AVAILABLE_MBProgressHUD
+#define COCOAPODS_VERSION_MAJOR_MBProgressHUD 0
+#define COCOAPODS_VERSION_MINOR_MBProgressHUD 6
+#define COCOAPODS_VERSION_PATCH_MBProgressHUD 0
+
+// MWPhotoBrowser
+#define COCOAPODS_POD_AVAILABLE_MWPhotoBrowser
+#define COCOAPODS_VERSION_MAJOR_MWPhotoBrowser 1
+#define COCOAPODS_VERSION_MINOR_MWPhotoBrowser 0
+#define COCOAPODS_VERSION_PATCH_MWPhotoBrowser 1
+
+// SDWebImage
+#define COCOAPODS_POD_AVAILABLE_SDWebImage
+#define COCOAPODS_VERSION_MAJOR_SDWebImage 2
+#define COCOAPODS_VERSION_MINOR_SDWebImage 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage 4
+
+// SDWebImage/Main
+#define COCOAPODS_POD_AVAILABLE_SDWebImage_Main
+#define COCOAPODS_VERSION_MAJOR_SDWebImage_Main 2
+#define COCOAPODS_VERSION_MINOR_SDWebImage_Main 7
+#define COCOAPODS_VERSION_PATCH_SDWebImage_Main 4
+
